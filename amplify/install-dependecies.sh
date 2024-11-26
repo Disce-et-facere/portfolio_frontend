8d0133cf779126f839e1412814ec63ac57102d8a
Robert Krantz <krantzr@hotmail.se>
2024-11-26 04:55:55 +0100

    modified:   amplify.yml 	deleted:    amplify/install-dependecies.sh

--- amplify/install-dependecies.sh
+++
@@ -1,9 +0,0 @@
-# install-dependencies.sh
-for dir in amplify/functions/*; do
-  if [ -d "$dir" ]; then
-    echo "Installing dependencies for $dir..."
-    cd "$dir"
-    npm install aws-sdk
-    cd -
-  fi
-done
