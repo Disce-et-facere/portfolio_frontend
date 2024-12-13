ae32667d0799ac19d3f40187003706a4b2ecdf76
Robert Krantz <krantzr@hotmail.se>
2024-12-13 20:07:08 +0100

    modified:   amplify/backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -7,8 +7,8 @@ import { createDevice } from './lambdas/createDevice/resource';
 import { deleteDevice } from './lambdas/deleteDevice/resource';
 import { fetchDeviceShadow } from './lambdas/fetchDeviceShadow/resource';
 
-const REGION = process.env.AWS_REGION; // placeholders if value is not set
 const ACCOUNT_ID = process.env.USER_ACCOUNT_ID; // placeholders if value is not set
+const REGION = process.env.AWS_REGION; // placeholders if value is not set
 const TABLE_NAME = process.env.DEVICE_TABLE_NAME; // placeholders if value is not set
 
 export const backend = defineBackend({
